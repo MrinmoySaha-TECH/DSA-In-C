b2dfff5316e71f0e5a37146cc7c786eebaaf12a3
MrinmoySaha-TECH <mrinmoysaha.tech@gmail.com>
2026-08-26 18:09:33 +0530

    Traversal Through A Linked list

---
+++ traversal-linked-list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+void linkedListTraversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element : %d \n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+int main()
+{
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+
+    // connect node
+    head->data = 8;
+    head->next = second;
+
+    second->data = 9;
+    second->next = third;
+
+    third->data = 10;
+    third->next = fourth;
+
+    fourth->data = 11;
+    fourth->next = NULL;
+
+    linkedListTraversal(head);
+
+    return 0;
+}
