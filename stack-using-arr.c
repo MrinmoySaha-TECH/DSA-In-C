bb4324d750c6cafad776eace7c59b239269f80fb
MrinmoySaha-TECH <mrinmoysaha.tech@gmail.com>
2026-09-19 18:14:04 +0530

    added stackTop and stackBottom Operation

--- stack-using-arr.c
+++ stack-using-arr.c
@@ -34,25 +34,52 @@ int isFull(struct stack *ptr)
 
 void push(struct stack *ptr, int value)
 {
-    if(isFull(ptr))
+    if (isFull(ptr))
     {
         printf("stack overflow , can't push element %d", value);
-    } else{
+    }
+    else
+    {
         ptr->top++;
         ptr->arr[ptr->top] = value;
     }
 }
 
-int pop (struct stack *ptr)
+int pop(struct stack *ptr)
 {
-     if(isEmpty(ptr))
+    if (isEmpty(ptr))
     {
-        printf("stack underflow , can't pop element from stack");
-    } else{
+        printf("stack underflow , can't pop element from stack\n");
+    }
+    else
+    {
         int val = ptr->arr[ptr->top];
         ptr->top--;
         return val;
     }
+}
+
+int peek(struct stack *p, int i)
+{
+    int arr = p->top - i + 1;
+    if (arr < 0)
+    {
+        printf("Not a valid Position");
+    }
+    else
+    {
+        return p->arr[arr];
+    }
+}
+
+int stackTop(struct stack *sp)
+{
+    return sp->arr[sp->top];
+}
+
+int stackBottom(struct stack *sp)
+{
+    return sp->arr[0];
 }
 
 int main()
@@ -63,11 +90,11 @@ int main()
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
 
-    printf("stack created\n\n");
-    printf("Stack before update \n");
-    printf("Stack is empty(1) or not(0) : %d\n",isEmpty(s));
-    printf("Stack is full(1) or not(0) : %d\n\n",isFull(s));
-    
+    // printf("stack created\n\n");
+    // printf("Stack before update \n");
+    // printf("Stack is empty(1) or not(0) : %d\n", isEmpty(s));
+    // printf("Stack is full(1) or not(0) : %d\n\n", isFull(s));
+
     push(s, 1);
     push(s, 2);
     push(s, 3);
@@ -79,9 +106,14 @@ int main()
     push(s, 9);
     push(s, 10);
     push(s, 11);
-    
-    printf("Stack after update :\n");
-    printf("Stack is empty(1) or not(0) : %d\n",isEmpty(s));
-    printf("Stack is full(1) or not(0) : %d\n",isFull(s));
+    // printf("Stack after update :\n");
+    // printf("Stack is empty(1) or not(0) : %d\n", isEmpty(s));
+    // printf("Stack is full(1) or not(0) : %d\n", isFull(s));
+
+    // printf("%d\n", peek(s, 1));
+
+    printf("bottom element of stack : %d\n", stackBottom(s));
+    printf("top element of stack : %d", stackTop(s));
+
     return 0;
 }
